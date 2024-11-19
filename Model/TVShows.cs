25eb6c29a298e8ea8e18b852c99feabcfea03039
JorgenMJobloop <jorgen@jobloop.no>
2024-11-19 10:31:39 +0100

    Add files via upload

---
+++ Model/TVShows.cs
@@ -0,0 +1,16 @@
+public class TVShows
+{
+    /// <summary>
+    /// Our TVShow endpoint model, that contain the following fields
+    /// int Id
+    /// string Title
+    /// string Creators
+    /// int ReleaseYear
+    /// int NumberOfSeasons
+    /// </summary>
+    public required int Id { get; set; }
+    public required string Title { get; set; }
+    public required string Creators { get; set; }
+    public required int ReleaseYear { get; set; }
+    public required int NumberOfSeasons { get; set; }
+}
